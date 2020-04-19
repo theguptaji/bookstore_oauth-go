16359be9aee6c49e84470b651aa2c41ccc790c57
Aman Gupta <aman@keio.jp>
2020-04-20 02:59:27 +0900

    refactoring

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -83,6 +83,9 @@ func AuthenticateRequest(request *http.Request) *errors.RestErr {
 	}
 	at, err := getAccessToken(accessTokenId)
 	if err != nil {
+		if err.Status == http.StatusNotFound {
+			return nil
+		}
 		return err
 	}
 
